f13c6d127b951312d14b729d4656d712ebb5f2ea
Nadersmith <105517789+Nadersmith@users.noreply.github.com>
2025-06-06 18:27:13 +0300

    logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,12 @@
+# utils/logger.py
 
+import logging
+
+def setup_logger():
+    logger = logging.getLogger("Genesis")
+    logger.setLevel(logging.INFO)
+    if not logger.handlers:
+        ch = logging.StreamHandler()
+        ch.setFormatter(logging.Formatter('[%(asctime)s] %(levelname)s - %(message)s'))
+        logger.addHandler(ch)
+    return logger
